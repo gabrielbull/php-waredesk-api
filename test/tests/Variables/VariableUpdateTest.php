2ab0844c6a551b549b660dadea904c945b3cddd5
Gabriel Bull <me@gabrielbull.com>
2017-06-15 11:21:39 -0400

    Added test for updating variables elements

--- test/tests/Variables/VariableUpdateTest.php
+++ test/tests/Variables/VariableUpdateTest.php
@@ -13,9 +13,12 @@ class VariableUpdateTest extends BaseTest
         $variable = $this->waredesk->variables->fetchOne();
         $variable->setName('Black T-Shirt Serial Number');
 
+        $variable->getElements()->first()->setValue('Amazing');
+
         $this->mock->append(new Response(200, [], file_get_contents(__DIR__ . '/responses/updateTestSuccess.json')));
         $variable = $this->waredesk->variables->update($variable);
 
         $this->assertEquals('Black T-Shirt Serial Number', $variable->getName());
+        $this->assertEquals('Amazing', $variable->getElements()->first()->getValue());
     }
 }
